08353a3174ab31cf820204a3dc85706c43eaf3c3
Alexander <alexmenshikov7@gmail.com>
2025-04-22 17:40:13 +0500

    created redux store

---
+++ src/app/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cardsApi } from './cardsApi'
+
+export const store = configureStore({
+    reducer: {
+        [cardsApi.reducerPath]: cardsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cardsApi.middleware)
+});
+    
+
+export type TypeRootState = ReturnType<typeof store.getState>
+
