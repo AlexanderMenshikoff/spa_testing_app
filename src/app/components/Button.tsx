abdf5b597bd42f7928f86fccf8c043aa192034cb
Alexander <alexmenshikov7@gmail.com>
2025-04-24 00:56:11 +0500

    added typization for button, added styles

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,15 +1,22 @@
+"use client"
+import { FC } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { IButtonData } from '../interfaces/interface';
 
-const Button = (props) => {
+const Button: FC<IButtonData> = ({ btnName, path }) => {
   const router = useRouter();
 
   const handleBack = () => {
-    return props.btnName === 'Назад' && router.back();
+    return btnName === 'Назад' && router.back();
   };
   return (
-    <Link href={props.path} id={props.id} scroll={false}>
-      <button onClick={handleBack}>{props.btnName}</button>
+    <Link href={path} scroll={false}>
+      <div className="absolute bottom-5 py-2 px-5 bg-black text-white rounded-xl">
+        <button className="cursor-pointer" onClick={handleBack}>
+          {btnName}
+        </button>
+      </div>
     </Link>
   );
 };
