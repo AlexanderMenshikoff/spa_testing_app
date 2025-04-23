72214f8a080fecaeebad0cc4781d75817fa47e9c
Alexander <alexmenshikov7@gmail.com>
2025-04-24 00:50:06 +0500

    used prettier & added get current user endpoint

--- src/app/redux/cardsApi.ts
+++ src/app/redux/cardsApi.ts
@@ -1,17 +1,26 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { ICard } from "../interfaces/interface";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { ICard, IUserRoot } from '../interfaces/interface';
 
 export const cardsApi = createApi({
-    reducerPath:'cardsApi',
-    baseQuery:fetchBaseQuery({baseUrl:'https://jsonplaceholder.typicode.com/'}),
-    endpoints: (build) => ({
-        getPosts: build.query<ICard[], number>({
-            query: (limit:number) => `posts?limit=${limit}`
-        }),
-        getCurrentPost: build.query<ICard, number>({
-            query: (id:number) => `posts/${id}`
-        }),
-    })
-})
+  reducerPath: 'cardsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
+  }),
+  endpoints: (build) => ({
+    getPosts: build.query<ICard[], number>({
+      query: (limit: number) => `posts?limit=${limit}`,
+    }),
+    getCurrentPost: build.query<ICard, number>({
+      query: (id: number) => `posts/${id}`,
+    }),
+    getCurrentUser: build.query<IUserRoot, number>({
+      query: (id: number) => `users/${id}`,
+    }),
+  }),
+});
 
-export const {useGetPostsQuery, useGetCurrentPostQuery} = cardsApi
+export const {
+  useGetPostsQuery,
+  useGetCurrentPostQuery,
+  useGetCurrentUserQuery,
+} = cardsApi;
