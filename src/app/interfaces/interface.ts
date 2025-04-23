95a1bb7e84db985bdf317a1f1b72b5f9a1edfe2e
Alexander <alexmenshikov7@gmail.com>
2025-04-24 00:50:29 +0500

    added new interfaces for user and button

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -1,7 +1,45 @@
-
 export interface ICard {
-    userId?: number;
-    id: number;
-    title: string;
-    body: string;
+  userId?: number;
+  id?: number;
+  title?: string;
+  body?: string;
+  userName?: string;
+  cardTitleStyle?: string;
+  cardBodyStyle?: string;
+}
+
+export interface IUserRoot {
+  id?: number;
+  name?: string;
+  username?: string;
+  email?: string;
+  address?: IUserAddress;
+  phone?: string;
+  website?: string;
+  company?: IUserCompany;
+}
+
+export interface IUserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IUserGeo;
+}
+
+export interface IUserGeo {
+  lat: string;
+  lng: string;
+}
+
+export interface IUserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface IButtonData {
+  path: string;
+  btnName: string;
+  btnStyle?: string;
 }
